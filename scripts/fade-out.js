041778bcd80857705127ca64698898c31aae1aa2
prestonline <269348742+prestonline@users.noreply.github.com>
2026-03-20 23:44:40 -0500

    feat(site): add page transitions

---
+++ scripts/fade-out.js
@@ -0,0 +1,41 @@
+const a = document.getElementsByTagName("a");
+
+for (let i = 0; i < a.length; i++) {
+  if (a.item(i).classList.value.includes(" home")) continue;
+  a.item(i).onclick = async function (event) {
+    event.preventDefault();
+    document.getElementById("container").classList.value += " fade-out";
+    // Source - https://stackoverflow.com/a/39914235
+    // Posted by Dan Dascalescu, modified by community. See post 'Timeline' for change history
+    // Retrieved 2026-03-20, License - CC BY-SA 4.0
+
+    await new Promise((r) => setTimeout(r, 1000));
+    document.getElementById("container").remove();
+
+    window.location.href = a.item(i).href;
+
+    return false;
+  };
+}
+
+const h = document.getElementsByClassName("home");
+
+for (let i = 0; i < h.length; i++) {
+  h.item(i).onclick = async function (event) {
+    event.preventDefault();
+    document.getElementById("background").classList.value =
+      "background-image fade-out";
+    document.getElementById("container").classList.value += " fade-out";
+    // Source - https://stackoverflow.com/a/39914235
+    // Posted by Dan Dascalescu, modified by community. See post 'Timeline' for change history
+    // Retrieved 2026-03-20, License - CC BY-SA 4.0
+
+    await new Promise((r) => setTimeout(r, 1000));
+
+    document.getElementById("container").remove();
+    document.getElementById("background").remove();
+    window.location.href = "index.html";
+
+    return false;
+  };
+}
